7e06af2183aebd4f6c352c2d395f66c3c259e7fa
Ashtaga <36531490+Ashtaga@users.noreply.github.com>
2019-04-30 00:14:15 +0200

    Add files via upload
    Modification du JS pour changer d'animation avec le thème

--- js/themes.js
+++ js/themes.js
@@ -5,20 +5,24 @@ function switchTheme(index){
 	switch (index){
 		case 0 :
 			body.style.backgroundColor = "#172935";
-			header.style.backgroundImage = 'linear-gradient(to top, #172935, #0498A2 80%)';
-			table.style.backgroundImage = 'linear-gradient(to top, #172935, #0498A2 25%)';
+			header.style.animation = "couleur0 5s infinite alternate-reverse";
+			table.style.animation = "couleur0 5s infinite alternate-reverse";
 			break;
 
 		case 1 :
 			body.style.backgroundColor = "#cc0000";
-			header.style.backgroundImage = 'linear-gradient(to top, #cc0000, #ff4d4d 80%)';
-			table.style.backgroundImage = 'linear-gradient(to top, #cc0000, #ff4d4d 25%)';
+			header.style.animation = "couleur1 5s infinite alternate-reverse";
+			table.style.animation = "couleur1 5s infinite alternate-reverse";
+			header.style.WebkitAnimation = "couleur1 5s infinite alternate-reverse";
+			table.style.WebkitAnimation = "couleur1 5s infinite alternate-reverse";
 			break;
 
 		case 2 :
 			body.style.backgroundColor = "#595959";
-			header.style.backgroundImage = 'linear-gradient(to top, #595959, #bfbfbf 80%)';
-			table.style.backgroundImage = 'linear-gradient(to top, #595959, #bfbfbf 25%)';
+			header.style.animation = "couleur2 5s infinite alternate-reverse";
+			table.style.animation = "couleur2 5s infinite alternate-reverse";
+			header.style.WebkitAnimation = "couleur2 5s infinite alternate-reverse";
+			table.style.WebkitAnimation = "couleur2 5s infinite alternate-reverse";
 			break;
 
 
