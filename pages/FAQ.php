83fddb730c92ba155d7c842e01bf62d5f1741723
GTRNexus <GTRNexus@users.noreply.github.com>
2019-05-05 21:30:41 +0200

    Update FAQ.php

--- pages/FAQ.php
+++ pages/FAQ.php
@@ -20,16 +20,14 @@ $html = '<!DOCTYPE html>
 
 		<header id="header">
 			<h1 id="titre">BLAIREAUX vs KÉKÉS</h1>
-				<form  id="theme" method="post">
-			  <fieldset id="changetheme">
-				<legend>Theme : </legend>
+				
+				<span>Theme : </span>
 				<select id="themes" onchange="switchTheme(this.selectedIndex)">
 					<option selected="selected">Défaut</option>
 					<option>Corail</option>
 					<option>Hiver</option>
 				</select>
-			  </fieldset>
-			</form>
+			  
 		</header>
 
 		<img src="../img/badger.png" align="center"/>';
